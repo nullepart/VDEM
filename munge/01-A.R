58f122c1a4f67e687e8e1388eff0fef6df5a950e
nullepart <fpdube@yahoo.ca>
2013-12-05 21:23:02 -0500

    Début de l'analyse des variables suffrage masculin et féminin.

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,12 @@
-# Example preprocessing script.
+library(stringr)
+
+
+# Attention: ici, je veux préparer un td sans les variables _conf, en utilisant
+# le modèle ci-après.
+pattern <- "a.b"
+strings <- c("abb", "a.b")
+str_detect(strings, pattern)
+str_detect(strings, fixed(pattern))
+
+vec <- c("test", "test_conf")
+!(str_detect(vec, fixed("_conf")))
